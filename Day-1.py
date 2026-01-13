c3e1c080280aaf45eeab80f2975dddaee72e1493
Ashrayrao5 <ashrayrao5@gmail.com>
2026-01-12 19:31:00 -0500

    adding the day-1 file

---
+++ Day-1.py
@@ -0,0 +1,42 @@
+# This is just a greeting to the file.
+print('Hello, Welcome to the world of Python!')
+
+
+# Sample creating a variable and printing the name and age.
+name = "Ashray"
+age = '24'
+print(name)
+print(age)
+print ('Hello Everyone! This is', name,'\nI am', age, 'years old.')
+
+
+# Create a variable for two numbers
+
+a = 5
+b= 10
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+
+# comparison operators
+x = 5
+
+print(x == 5)
+print(x != 5)
+print(x > 5)
+print(x < 5)
+
+# Logical Operators
+y=10
+print(x<10 and y>5)
+print(x<10 and y<5)
+print(not(x==5))
+
+# jdfnbijan
+# ndfobno
