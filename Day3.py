71451dbabf92d19c9b6d402cab3d378edbf1ef31
Ashrayrao5 <ashrayrao5@gmail.com>
2026-01-14 17:54:27 -0500

    Work on this

---
+++ Day3.py
@@ -0,0 +1,36 @@
+# fruits = ["apple", "banana", "cherry"]
+#
+# fruits.insert(10, "Blueberry")
+# print(fruits[3])
+
+#
+# list = [1,2,2,3,3,4,4,4,4,5,6,7,7,8,9]
+# abc = set(list)
+# print(abc)
+
+#
+# person = dict(name = 'John', age = 20, city = 'New York')
+# print(person)
+
+
+person_1 = {"name" :"John", "age" : 20, "city":'New York'}
+print(person_1)
+
+person_2 = {}
+person_2["name"] = "Sarah"
+person_2["age"] = 24
+person_2["city"] = "Boston"
+person_2["email"] ="abc.email.com"
+print(person_2)
+
+# Nested Dictionaries
+
+# practice list, tuples, set and dict
+# More work on syntax and other use cases
+
+# Home-work
+# Practice the Dictionaries
+# insert
+# delete
+
+# Do any research on the API for data Analytics purposes
